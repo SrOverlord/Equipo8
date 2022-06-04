da1133c9000a08a5323b30359378286d047bbbc5
SrOverlord <franciscoa2310.pm@gmail.com>
2022-06-04 19:24:19 +0200

    Pruab7

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus"
 //=require activestorage
 //=require apexcharts
+//=require chartstick
+//=require Chart.bundle
 
 const application = Application.start()
 
