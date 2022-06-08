d57cdf5ac36c9ca5cf37ac125be51440e8487cea
SrOverlord <franciscoa2310.pm@gmail.com>
2022-06-08 14:55:34 +0200

    Memtemos vista de user y cuenta banco

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -48,7 +48,7 @@ module Api
         end
         private
         def user_params
-              params.require(:user).permit(:id, :email, :encrypted_password, :nombre, :apellidos, :dni, :telefono, :contrasena)
+              params.require(:user).permit( :email, :encrypted_password, :nombre, :apellidos, :dni, :telefono, :contrasena)
         end
         end
         end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     namespace :v1 do
      resources :projects
      resources :cuenta_bancos
-     resources :users
+     devise_for :users
           end
    end
 
