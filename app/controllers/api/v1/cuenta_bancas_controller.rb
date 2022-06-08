12ac49c1d85d54ef7fca5368d649f92826322659
SrOverlord <franciscoa2310.pm@gmail.com>
2022-06-09 00:12:03 +0200

    Memtemos vista de user y cuenta banco

--- app/controllers/api/v1/cuenta_bancas_controller.rb
+++ app/controllers/api/v1/cuenta_bancas_controller.rb
@@ -6,7 +6,7 @@ module Api
   skip_before_action :verify_authenticity_token
   
   def show
-        @cuenta_banca = CuentaBanca.where(user_id: params[:id])
+        @cuenta_banca = CuentaBanca.where(user_id: params[:id], id: params[:id])
         if (@cuenta_banca!=nil)
           render json: @cuenta_banca, status: :ok
     else
