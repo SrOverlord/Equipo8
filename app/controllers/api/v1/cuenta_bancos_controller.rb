527fa0502f0a7dde0d5347eb2f9e67b40b269a07
SrOverlord <franciscoa2310.pm@gmail.com>
2022-06-08 13:50:21 +0200

    Memtemos vista de user y cuenta banco

--- app/controllers/api/v1/cuenta_bancos_controller.rb
+++ app/controllers/api/v1/cuenta_bancos_controller.rb
@@ -1,55 +1,56 @@
+
 module Api
   module V1
-class  CuentaBancosController < ApplicationController
+class CuentaBancosController < ApplicationController
   skip_before_action :verify_authenticity_token
     
-  def show
-        @cuenta_bancos = CuentaBanco.where("where user_id = "+ params[:id])
-        if (@cuenta_bancos!=nil)
-          render json: @cuenta_bancos, status: :ok
-    else
-         render json: :nothing, status: :not_found
+      def show
+            @cuenta_banco = CuentaBanco.where("user_id = " + params[:id])
+            if (@cuenta_banco!=nil)
+              render json: @cuenta_banco, status: :ok
+          else
+             render json: :nothing, status: :not_found
+          end
       end
-  end
-  
-  def index
-          render json: CuentaBanco.all, status: :ok
-  end
-  
-  def update
-   @cuenta_bancos = CuentaBanco.find(params[:id])
-  
-    if @cuenta_bancos.update(project_params)
-          render json: @cuenta_bancos, status: :ok
-        else
-          render json: @cuenta_bancos.errors, status: :unprocessable_entity
-        end
-  end
-  
-  def create
-      @cuenta_bancos = CuentaBanco.new(project_params)
-  
-        if @cuenta_bancos.save
-          render json: @cuenta_bancos, status: :created
-        else
-          render json: @cuenta_bancos.errors, status: :unprocessable_entity
-        end
-  end
-  
-  def destroy
-      @cuenta_bancos = CuentaBanco.find(params[:id])
-      if @cuenta_bancos.destroy
-        render json: :nothing, status: :ok
-      else
-        render json: :nothing, status: :unprocessable_entity
+      
+      def index
+              render json: CuentaBanco.all, status: :ok
       end
-  end
-  private
-  def project_params
-    params.require(:cuenta_banco).permit(:users_id, :numeroCuenta, :saldo, :tipo, :comision, :divisa)
-  end
-  end
-  end
-  end
-  
-  
+      
+      def update
+       @cuenta_banco = CuentaBanco.find(params[:id])
+      
+        if @cuenta_banco.update(project_params)
+              render json: @cuenta_banco, status: :ok
+            else
+              render json: @cuenta_banco.errors, status: :unprocessable_entity
+            end
+      end
+      
+      def create
+          @cuenta_banco = CuentaBanco.new(project_params)
+      
+            if @cuenta_banco.save
+              render json: @cuenta_banco, status: :created
+            else
+              render json: @cuenta_banco.errors, status: :unprocessable_entity
+            end
+      end
+      
+      def destroy
+          @cuenta_banco = CuentaBanco.find(params[:id])
+          if @cuenta_banco.destroy
+            render json: :nothing, status: :ok
+          else
+            render json: :nothing, status: :unprocessable_entity
+          end
+      end
+      private
+      def project_params
+        params.require(:cuenta_banco).permit(:users_id, :numeroCuenta, :saldo, :tipo, :comision, :divisa)
+      end
+      end
+      end
+      end
+      
+      
