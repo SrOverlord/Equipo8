766574d8a02daf95279eed4488c3d724110a2908
FranPino <franciscoa2310.pm@gmail.com>
2022-06-08 17:02:43 +0000

    Lo nuevo

--- db/schema.rb
+++ db/schema.rb
@@ -78,15 +78,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_08_102508) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "usuarios", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.integer "team"
-    t.text "info"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["team"], name: "index_usuarios_on_team", unique: true
-  end
-
   add_foreign_key "cuenta_bancos", "users", column: "users_id"
   add_foreign_key "developers", "projects"
   add_foreign_key "pruebas", "projects"
