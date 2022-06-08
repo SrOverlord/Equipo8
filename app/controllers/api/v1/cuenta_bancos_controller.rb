9f28713093a6f585e78cae698049156af72c76af
SrOverlord <franciscoa2310.pm@gmail.com>
2022-06-08 12:43:26 +0200

    Memtemos vista de user y cuenta banco

--- app/controllers/api/v1/cuenta_bancos_controller.rb
+++ app/controllers/api/v1/cuenta_bancos_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-class  < ApplicationController
+class  CuentaBancosController < ApplicationController
   skip_before_actioCuentaBancosControllern :verify_authenticity_token
     
   def show
