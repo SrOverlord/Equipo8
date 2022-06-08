7fe45e4e080b7f6bece58818b3042a04484fdacd
SrOverlord <franciscoa2310.pm@gmail.com>
2022-06-08 13:44:04 +0200

    Memtemos vista de user y cuenta banco

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,57 +1,57 @@
 
 module Api
-  module V1
-    class UsersController < ApplicationController
-
-      skip_before_action :verify_authenticity_token
-    
-      def show
-            @user = User.find_by(id: params[:id])
-            if (@user!=nil)
-              render json: @user, status: :ok
-        else
-             render json: :nothing, status: :not_found
-          end
-      end
+    module V1
+      class UsersController < ApplicationController
+  
+        skip_before_action :verify_authenticity_token
       
-      def index
-              render json: User.all, status: :ok
-      end
-      
-      def update
-       @user = User.find(params[:id])
-      
-        if @user.update(project_params)
-              render json: @user, status: :ok
-            else
-              render json: @user.errors, status: :unprocessable_entity
-            end
-      end
-      
-      def create
-          @user = User.new(project_params)
-      
-            if @user.save
-              render json: @user, status: :created
-            else
-              render json: @user.errors, status: :unprocessable_entity
-            end
-      end
-      
-      def destroy
-          @user = User.find(params[:id])
-          if @user.destroy
-            render json: :nothing, status: :ok
+        def show
+              @user = User.find_by(id: params[:id])
+              if (@user!=nil)
+                render json: @user, status: :ok
           else
-            render json: :nothing, status: :unprocessable_entity
-          end
-      end
-      private
-      def project_params
-            params.require(:user).permit( :email, :encrypted_password, :nombre, :apellidos, :dni, :telefono, :contrasena)
-      end
-      end
-      end
-      end
-      
-      
+               render json: :nothing, status: :not_found
+            end
+        end
+        
+        def index
+                render json: User.all, status: :ok
+        end
+        
+        def update
+         @user = User.find(params[:id])
+        
+          if @user.update(project_params)
+                render json: @user, status: :ok
+              else
+                render json: @user.errors, status: :unprocessable_entity
+              end
+        end
+        
+        def create
+            @user = User.new(project_params)
+        
+              if @user.save
+                render json: @user, status: :created
+              else
+                render json: @user.errors, status: :unprocessable_entity
+              end
+        end
+        
+        def destroy
+            @user = User.find(params[:id])
+            if @user.destroy
+              render json: :nothing, status: :ok
+            else
+              render json: :nothing, status: :unprocessable_entity
+            end
+        end
+        private
+        def project_params
+              params.require(:user).permit( :email, :encrypted_password, :nombre, :apellidos, :dni, :telefono, :contrasena)
+        end
+        end
+        end
+        end
+        
+          
