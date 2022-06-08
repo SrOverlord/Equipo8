40bcca2a3ff8c0f897c4c472913aa330fcde2f09
SrOverlord <franciscoa2310.pm@gmail.com>
2022-06-08 20:48:46 +0200

    Memtemos vista de user y cuenta banco

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,28 @@
 Rails.application.routes.draw do
-  resources :cuenta_bancas
+  resources :cuenta_bancos
   devise_for :users
-  root 'main#home'
+  resources :users
+  # resources :cuenta_bancos
+  # resources :users
+  resources :perros
+  # get 'porjects/index'
+  resources :developers
+  resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+	
   namespace :api, defaults: { format: 'json' }  do
     namespace :v1 do
-     resources :cuenta_bancas
+     resources :projects
+     resources :cuenta_bancos
      resources :users
+     devise_for :users
           end
    end
+
+   
+  root to: 'projects#index'
 
 end
