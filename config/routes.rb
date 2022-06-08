b8bdc082d8773b3e2c2fc253c3f5096b9e43d425
SrOverlord <franciscoa2310.pm@gmail.com>
2022-06-08 21:34:17 +0200

    Memtemos vista de user y cuenta banco

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :cuenta_bancos
+  resources :cuenta_bancas
   devise_for :users
   resources :users
   # resources :cuenta_bancos
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' }  do
     namespace :v1 do
      resources :projects
-     resources :cuenta_bancos
+     resources :cuenta_bancas
      resources :users
      devise_for :users
           end
